2a241ccfbbda14d69c00794cf0d3a2a02b4d61de
katarine <mksb@cin.ufpe.br>
2023-12-05 22:53:40 -0300

    Melhorando o servidor HTTPs

--- Segurança/Servidor_Web_https.py
+++ Segurança/Servidor_Web_https.py
@@ -3,13 +3,27 @@
 import http.server
 import ssl
 
-httpd = http.server.HTTPServer(('localhost', 443),
-                               http.server.SimpleHTTPRequestHandler)
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # Adiciona um log no terminal mostrando que o usuário realizou uma solicitação
+        print(f"Usuário fez uma solicitação: {self.path}")
 
+        # Retorna um texto HTML simples para o cliente
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'<html><body><h1>Hello, World!</h1></body></html>')
+
+# Cria um servidor HTTP com certificados SSL
+httpd = http.server.HTTPServer(('localhost', 443), MyHandler)
+
+# Configuração do SSL
 httpd.socket = ssl.wrap_socket(httpd.socket,
                                certfile='certificate.pem',
-                               keyfile= "key.pem",
+                               keyfile='key.pem',
                                server_side=True,
                                ssl_version=ssl.PROTOCOL_TLS)
 
+# Inicia o servidor
+print("Servidor HTTPS rodando na porta 443...")
 httpd.serve_forever()
